b9ed0b44f6dcd9d85122b4a2a9e8963a81c0e559
Mangesh Panchwagh <panchwaghmangesh177@gmail.com>
2024-10-10 21:47:54 +0530

    Change

--- src/stringPrograms/reverseString1.java
+++ src/stringPrograms/reverseString1.java
@@ -6,16 +6,16 @@ public class reverseString1 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Enter the String : ");
+		System.out.println("Enter the String for reverse : ");
 		Scanner scan = new Scanner(System.in);
 		String s = scan.nextLine();
-
+		
 		String revString = "";
-
+		System.out.println("Entered String is : " + s);
+		
 		for (int i = s.length() - 1; i >= 0; i--) {
 			revString += s.charAt(i);
 		}
-
 		System.out.println("Revese String is : " + revString);
 	}
 
